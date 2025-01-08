883f36942cd050fae2bb7003d76061dfea837edb
Stuart Rackham <srackham@gmail.com>
2025-01-08 15:31:23 +1300

    check for HTTP status 200

--- main.go
+++ main.go
@@ -26,6 +26,10 @@ func GetPrice(symbol string, get httpGet) (float64, error) {
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("unexpected HTTP response status code: %d", resp.StatusCode)
+	}
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return 0, fmt.Errorf("error reading response: %v", err)
